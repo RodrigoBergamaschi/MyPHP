16a935fefacd6e749129adbaee25a7474e1d6c51
Wesley616 <56004346+Wesley616@users.noreply.github.com>
2019-11-06 21:06:14 -0300

    06/11

--- views/admin/layout/mainSideBar.php
+++ views/admin/layout/mainSideBar.php
@@ -27,21 +27,27 @@
             <a href="#" class="nav-link active">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
-                Starter Pages
+               Cadastros
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="#" class="nav-link active">
+                <a href="#" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Active Page</p>
+                  <p>Clientes</p>
                 </a>
               </li>
               <li class="nav-item">
                 <a href="#" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Inactive Page</p>
+                  <p>Parceiros</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="fornecedor.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Fornecedores</p>
                 </a>
               </li>
             </ul>
